3b2726d61ac19c8df00c95810f29c3b996980b36
chinsun9 <chin_sung@naver.com>
2021-03-25 16:21:25 +0900

    refactor unused import

--- src/component/Start.tsx
+++ src/component/Start.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Page } from '../types';
 
 type Props = {
