e45b96404fbb594aa13cfa429054fb54c06fb8ef
chinsun9 <chin_sung@naver.com>
2021-05-24 13:12:03 +0900

    refactor css

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -14,16 +14,6 @@ function Main({ setPage }: Props) {
     <>
       <h1>반응속도 테스트</h1>
 
-      <hr />
-      <div className="notice">
-        <h2>유의사항</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas
-          voluptates cum unde consequatur assumenda cupiditate incidunt dolore
-          enim, nostrum deleniti id recusandae dignissimos delectus pariatur
-          blanditiis dolorum adipisci necessitatibus. Natus!
-        </p>
-      </div>
       <button type="button" onClick={onClickHandler}>
         테스트 시작
       </button>
